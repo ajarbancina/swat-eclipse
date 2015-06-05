59fb6932ad5a28c93e75edcc35439ab21dd17fb2
hawklorry <hawklorry@gmail.com>
2015-06-05 21:11:15 +0000

    CanSWAT2012: Update from 622 to 627 through merging changes from SWAT2012.

--- CanSWAT2012/allocate_parms.f
+++ CanSWAT2012/allocate_parms.f
@@ -64,6 +64,7 @@
       motot = 600             !! (50 years limit)
       mmpoint=1    !Liu
       
+      allocate (surlag(mhru))
 !!!!!!!!!! drains
       allocate (wnan(mlyr))
 
@@ -511,6 +512,8 @@
       allocate (wat_tbl(mhru))     
       allocate (sol_swpwt(mhru))
       allocate (conk(mlyr,mhru))
+	allocate (r2adj(mhru))	        
+      allocate (sol_stpwt(mlyr,mhru)) !Moriasi 4/8/2014
 !    Drainmod tile equations  01/2006
       allocate (conv_wt(mlyr,mhru))
       allocate (crdep(mlyr,mhru))
@@ -1196,6 +1199,7 @@
       allocate (potlagi(mhru))
 
       allocate (pplnt(mhru))
+      allocate (prf(mch))  !Moriasi 4/8/14    
       allocate (qdr(mhru))
       allocate (qdayout(mhru))
       allocate (rch_dakm(mxsubch))  
